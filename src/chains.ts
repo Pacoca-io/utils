81c5e91a8d1a833c53c68d000cb0a006d89b151d
AmendoBobo <amendobobo404@protonmail.com>
2022-07-06 15:53:34 -0300

    feat: tests

--- src/chains.ts
+++ src/chains.ts
@@ -1,5 +1,5 @@
 import { chainData, ETH, BSC, POLY, FTM, AVAX } from './constants/chains'
 
-export const availableChains = [ETH, BSC, POLY, FTM, AVAX]
-
 export type Chain = keyof typeof chainData
+
+export const availableChains: Chain[] = [ETH, BSC, POLY, FTM, AVAX]
